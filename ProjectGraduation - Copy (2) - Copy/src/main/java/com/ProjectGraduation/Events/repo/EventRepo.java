354a053d0c4066f0d11388498afeafb2e9a596f6
M7md fathey <mohamedfathey>
2025-02-17 01:08:00 +0300

    delete anthoer version commit

--- ProjectGraduation - Copy (2) - Copy/src/main/java/com/ProjectGraduation/Events/repo/EventRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ProjectGraduation.Events.repo;
-
-import com.ProjectGraduation.Events.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepo extends JpaRepository<Event,Long> {
-}
--- ProjectGraduation - Copy (2) - Copy/src/main/java/com/ProjectGraduation/auth/exception/UserAlreadyExistsException.java
+++
@@ -1,4 +0,0 @@
-package com.ProjectGraduation.auth.exception;
-
-public class UserAlreadyExistsException  extends Exception{
-}
